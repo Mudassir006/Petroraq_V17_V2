965c5eed9c6c0c549674a74b0cc2b58d9bdebd72
Mudassir006 <95249069+Mudassir006@users.noreply.github.com>
2026-05-07 14:26:58 +0300

    Add portal payslip downloads

--- de_hr_workspace_payroll/controllers/employee_payslip_portal.py
+++ de_hr_workspace_payroll/controllers/employee_payslip_portal.py
@@ -1,12 +1,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import base64
 from collections import OrderedDict
 from operator import itemgetter
 from markupsafe import Markup
 
 from odoo import conf, http, _
 from odoo.exceptions import AccessError, MissingError
-from odoo.http import request
+from odoo.http import content_disposition, request
 from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager
 from odoo.tools import groupby as groupbyelem
 
@@ -34,6 +35,27 @@ class EmployeePayslipPortal(CustomerPortal):
             'date_from': {'label': _('Newest'), 'order': 'date_from desc'},
             'date_to': {'label': _('Oldest'), 'order': 'date_from asc'},
         }
+
+    def _check_payslip_owner(self, payslip_id):
+        if payslip_id.employee_id.user_id != request.env.user:
+            raise MissingError(_("This payslip does not exist or you do not have access to it."))
+
+    def _get_payslip_attachment(self, payslip_id):
+        attachment = request.env['ir.attachment'].sudo().search([
+            ('res_model', '=', 'hr.payslip'),
+            ('res_id', '=', payslip_id.id),
+            ('type', '=', 'binary'),
+            ('mimetype', 'ilike', 'pdf'),
+        ], order='create_date desc, id desc', limit=1)
+        if not attachment:
+            attachment = request.env['ir.attachment'].sudo().search([
+                ('res_model', '=', 'hr.payslip'),
+                ('res_id', '=', payslip_id.id),
+                ('type', '=', 'binary'),
+            ], order='create_date desc, id desc', limit=1)
+        if not attachment:
+            raise MissingError(_("No payslip attachment is available for download."))
+        return attachment
 
     @http.route(['/my/payslips', '/my/payslips/page/<int:page>'], type='http', auth="user", website=True)
     def portal_my_payslips(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
@@ -78,11 +100,23 @@ class EmployeePayslipPortal(CustomerPortal):
 
     @http.route(['/my/payslips/<model("hr.payslip"):payslip_id>'], type='http', auth="user", website=True)
     def portal_my_payslips_payslip_info(self, payslip_id, **kw):
-        if payslip_id.employee_id.user_id != request.env.user:
-            raise MissingError(_("This payslip does not exist or you do not have access to it."))
+        self._check_payslip_owner(payslip_id)
 
         values = {
             "payslip_id": payslip_id.sudo(),
             "page_name": "payslip",
         }
         return request.render("de_hr_workspace_payroll.employee_payslip_info_portal", values)
+
+    @http.route(['/my/payslips/<model("hr.payslip"):payslip_id>/download'], type='http', auth="user", website=True)
+    def portal_my_payslips_download(self, payslip_id, **kw):
+        self._check_payslip_owner(payslip_id)
+
+        attachment = self._get_payslip_attachment(payslip_id)
+        filename = attachment.name or '%s.pdf' % (payslip_id.number or payslip_id.name or 'payslip')
+        content = base64.b64decode(attachment.datas or b'')
+        return request.make_response(content, [
+            ('Content-Type', attachment.mimetype or 'application/octet-stream'),
+            ('Content-Length', str(len(content))),
+            ('Content-Disposition', content_disposition(filename)),
+        ])
